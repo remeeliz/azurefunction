0d005c4cb77975b1bd326592136c157c319c8968
remeeliz <36454978+remeeliz@users.noreply.github.com>
2024-09-12 19:30:32 -0400

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -11,8 +11,6 @@ password = os.getenv("SQL_PASSWORD")
 #driver = '{ODBC Driver 17 for SQL Server}'
 
 driver = '{ODBC Driver 18 for SQL Server}'
-
-
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="remeehttp")
@@ -21,17 +19,14 @@ def remeehttp(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
         # Establish connection to SQL Server
-        conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
+        conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password};Authentication=ActiveDirectoryMsi')
         cursor = conn.cursor()
         
         # Example query
-        cursor.execute("SELECT TOP 10 [name] FROM sys.objects")
+        cursor.execute("select * from sys.objects for json auto")
         rows = cursor.fetchall()
-        
-        # Convert result to JSON
-        result = [dict(zip([column[0] for column in cursor.description], row)) for row in rows]
-        
-        return func.HttpResponse(json.dumps(result), status_code=200, mimetype="application/json")
+            
+        return func.HttpResponse(json.dumps(rows), status_code=200, mimetype="application/json")
     
     except Exception as e:
         return func.HttpResponse(f"Error: {str(e)}", status_code=500)
