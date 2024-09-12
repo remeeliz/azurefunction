24ea58dd3b9747f9d6a0961ed90b414765264714
remeeliz <36454978+remeeliz@users.noreply.github.com>
2024-09-12 19:43:00 -0400

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -19,15 +19,16 @@ def remeehttp(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
         # Establish connection to SQL Server
-        conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
-        #conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};Authentication=ActiveDirectoryMsi')
+        #conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
+        conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};Authentication=ActiveDirectoryMsi')
         cursor = conn.cursor()
         
         # Example query
         cursor.execute("select * from sys.objects for json auto")
         rows = cursor.fetchall()
+        result = [dict(zip([column[0] for column in cursor.description], row)) for row in rows]
             
-        return func.HttpResponse(json.dumps(rows), status_code=200, mimetype="application/json")
+        return func.HttpResponse(json.dumps(result), status_code=200, mimetype="application/json")
     
     except Exception as e:
         return func.HttpResponse(f"Error: {str(e)}", status_code=500)
